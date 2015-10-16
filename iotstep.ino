540d6cc5405a179bb063bbb52756bed38732fd8e
Shinnosuke Komiya <kurohune538@gmail.com>
2015-10-17 00:07:04 +0900

    ino追加

---
+++ iotstep.ino
@@ -0,0 +1,52 @@
+const int LED = 13;
+int rsvData = 0;
+int leftOrRight,thumb,child,kakato,accelX,accelY,accelZ,gyroX,gyroY,gyroZ,conpX,conpY,conpZ,airPress = 0;
+byte sendData[14];
+void setup()
+{
+  pinMode(LED,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  // 受信処理
+  if(Serial.available() > 0){
+    rsvData = Serial.read();
+    if(rsvData == 1){ 
+      digitalWrite(LED, HIGH);
+    }
+    else if(rsvData == 0){
+      digitalWrite(LED, LOW);
+    } 
+  } 
+
+  //センサー読み取り～送信
+  leftOrRight = 1;
+  thumb = analogRead(A0);
+  child = analogRead(A1);
+  kakato = analogRead(A2);
+  sendData[0] = leftOrRight;
+  sendData[1] = thumb;
+  sendData[2] = child;
+  sendData[3] = kakato;
+  sendData[4] = accelX;
+  sendData[5] = accelY;
+  sendData[6] = accelZ;
+  sendData[7] = gyroX;
+  sendData[8] = gyroY;
+  sendData[9] = gyroZ;
+  sendData[10] = conpX;
+  sendData[11] = conpY;
+  sendData[12] = conpZ;
+  sendData[13] = airPress;
+  Serial.println(thumb);
+  Serial.write(thumb);
+  //Serial.write(sendData,14);
+//  Serial.println(leftOrRight);
+//  Serial.println(child);
+  //ウェイト
+
+  delay(100);
+}
+
